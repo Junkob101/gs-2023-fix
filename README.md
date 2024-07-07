9ec349c9ce7a81287bada01a4bf7ce28fb320927
Junkob101 <39352890+Junkob101@users.noreply.github.com>
2024-07-06 22:26:49 -0400

    Updated ReadMe, fixed download for script.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # gs-2023-fix
+
+
+Downloads older overlay files for steam to fix Gamesense injection. Running will automatically (un)install the fix.
+
+
+
+Requires psutil & Python 3.
+
+## If you don't have PSUTIL, download using command below (run in cmd)
+```
+pip install psutil 
+```
--- overlayswap.py
+++ overlayswap.py
@@ -35,25 +35,25 @@ def work():
 		os.rename('GameOverlayRenderer64.dll', 'GameOverlayRenderer64.dll.old')
 		os.rename('GameOverlayUI.exe', 'GameOverlayUI.exe.old')
 		os.rename('GameOverlayRenderer.log', 'GameOverlayRenderer.log.old')
-		urllib.request.urlretrieve("https://github.com/Junkob101/gs-2023-fix/raw/main/GameOverlayRenderer.dll")
-		urllib.request.urlretrieve("https://github.com/Junkob101/gs-2023-fix/raw/main/GameOverlayRenderer.log")
-		urllib.request.urlretrieve("https://github.com/Junkob101/gs-2023-fix/raw/main/GameOverlayRenderer64.dll")
-		urllib.request.urlretrieve("https://github.com/Junkob101/gs-2023-fix/raw/main/GameOverlayUI.exe")
-		urllib.request.urlretrieve("https://github.com/Junkob101/gs-2023-fix/raw/main/steam.cfg")
+		urllib.request.urlretrieve("https://github.com/Junkob101/gs-2023-fix/raw/main/GameOverlayRenderer.dll", "GameOverlayRenderer.dll")
+		urllib.request.urlretrieve("https://raw.githubusercontent.com/Junkob101/gs-2023-fix/main/GameOverlayRenderer.log", "GameOverlayRenderer.log")
+		urllib.request.urlretrieve("https://github.com/Junkob101/gs-2023-fix/raw/main/GameOverlayRenderer64.dll", "GameOverlayRenderer64.dll")
+		urllib.request.urlretrieve("https://github.com/Junkob101/gs-2023-fix/raw/main/GameOverlayUI.exe", "GameOverlayUI.exe")
+		urllib.request.urlretrieve("https://raw.githubusercontent.com/Junkob101/gs-2023-fix/main/steam.cfg", "steam.cfg")
 
 	else:
 		if os.path.isfile('GameOverlayRenderer.dll.old') == True:
-                        os.remove('GameOverlayRenderer.dll')
-                        os.remove('GameOverlayRenderer64.dll')
-                        os.remove('GameOverlayUI.exe')
-                        os.remove('GameOverlayRenderer.log')
-                        os.rename('GameOverlayRenderer.dll.old', 'GameOverlayRenderer.dll')
-                        os.rename('GameOverlayRenderer64.dll.old', 'GameOverlayRenderer64.dll')
-                        os.rename('GameOverlayUI.exe.old', 'GameOverlayUI.exe')
-                        os.rename('GameOverlayRenderer.log.old', 'GameOverlayRenderer.log')
-                        os.remove('steam.cfg')
-                else:
-                        os.remove('steam.cfg')
+			os.remove('GameOverlayRenderer.dll')
+			os.remove('GameOverlayRenderer64.dll')
+			os.remove('GameOverlayUI.exe')
+			os.remove('GameOverlayRenderer.log')
+			os.rename('GameOverlayRenderer.dll.old', 'GameOverlayRenderer.dll')
+			os.rename('GameOverlayRenderer64.dll.old', 'GameOverlayRenderer64.dll')
+			os.rename('GameOverlayUI.exe.old', 'GameOverlayUI.exe')
+			os.rename('GameOverlayRenderer.log.old', 'GameOverlayRenderer.log')
+			os.remove('steam.cfg')
+		else:
+			os.remove('steam.cfg')
 
 
 #def main():
